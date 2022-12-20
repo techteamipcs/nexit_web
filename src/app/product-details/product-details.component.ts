d95a6a8989a0592001a55aead13a589be569b42b
Budesh <bpatil775@gmail.com>
2022-12-20 17:50:48 +0530

    code commit for related product slider and product count

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { DataService } from '../providers/data.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-product-details',
@@ -12,6 +13,32 @@ export class ProductDetailsComponent implements OnInit {
   productId: any;
   product: any;
   backendURL = '';
+  productSlider: OwlOptions = {
+    loop: true,
+    autoplay:true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    margin: 14,
+    responsive: {
+      0: {
+        items: 2
+      },
+      400: {
+        items: 3
+      },
+      740: {
+        items: 4
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: false
+  }
   constructor(public router: ActivatedRoute, public dataservice: DataService) {
     this.backendURL = environment.backendUrl+'/public/';
    }
