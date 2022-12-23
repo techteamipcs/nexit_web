bddb8c58f7e45bd3fc9cbf33606c3ab804533461
mohammed asad <asad.ipcs@gmail.com>
2022-12-23 09:58:45 +0530

    footer changes

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { DataService } from 'src/app/providers/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  allcategories: any = [];
+  allbrands: any = [];
+  baseUrl: any;
+  backendUrl: any;
+  currentYear = new Date().getFullYear(); // 2020
+  constructor(private dataservice: DataService, public route:Router) { 
+    this.baseUrl = environment.baseUrl + '/assets';
+    this.backendUrl = environment.backendUrl + '/public/';
   }
+  
+  ngOnInit(): void {
+    this.getAllCategory();
+  }
+  getAllCategory() {
+    this.dataservice.getAllCategory({}).subscribe(
+      (response) => {
+        if (response.code == 200) {
 
+          if (response.result && response.result.length > 0) {
+            this.allcategories = response.result;
+          }
+
+        } else if (response.code == 400) {
+
+        }
+        else {
+
+        }
+
+      },
+    );
+  }
 }
