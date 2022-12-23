ef61fba85f91fc72fc7b69be81b6142d6769d02f
mohammed asad <asad.ipcs@gmail.com>
2022-12-23 10:00:00 +0530

    about and service page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { BrandsComponent } from './brands/brands.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
 import { TermsAndConditionComponent } from './terms-and-condition/terms-and-condition.component';
 
 const routes: Routes = [
   {
     path:'',
     component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'services',
+    component:ServicesComponent
   },
   {
     path:'brands',
