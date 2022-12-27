0c3e2dc7e84a43d11617799835f1f8091f78096f
mohammed asad <asad.ipcs@gmail.com>
2022-12-27 18:17:15 +0530

    tab and mobile screen menu adding

--- src/assets/source/js/script.js
+++ src/assets/source/js/script.js
@@ -55,5 +55,61 @@ $(window).on('load', function () {
   //     }
   //   ]
   // });
+  if (typeof (window.Layout) == "undefined") {
+    window.Layout = new Object();
+}
+
+(function (Layout) {
+
+    //return
+    Layout.Init = Init;
+
+    function Init() {
+        InitMainNav();
+    }
+
+    function InitMainNav() {
+        var mainNav$ = $('#siteNav .mainNav:first');
+        var burger$ = $('#mainNavBurger');
+        var nodeToggles$ = mainNav$.find('.toggle');
+        burger$.on('click', function () {
+            if ($(this).hasClass('open')) {
+                nodeToggles$.removeClass('open');
+                burger$.removeClass('open');
+                mainNav$.removeClass('open');
+            } else {
+                burger$.addClass('open');
+                mainNav$.addClass('open');
+            }
+        });
+        nodeToggles$.on('click', function (e) { _handleClick(e, false); });
+        nodeToggles$.on('keydown', function (e) { _handleClick(e, true); });
+      
+
+        function _handleClick(e, isKeyPress) {
+            var key = e.keyCode || e.which;
+            if (isKeyPress && key !== 13) { return; }
+            e.preventDefault();
+            var this$ = $(e.currentTarget);
+            var sibToggles$ = this$.closest('li').siblings('li').find('.toggle');
+            var kidToggles$ = this$.siblings('ul').find('.toggle');
+            if (this$.hasClass('open')) {
+                sibToggles$.removeClass('open');
+                kidToggles$.removeClass('open');
+                this$.removeClass('open');
+            } else {
+                sibToggles$.not(this$).removeClass('open');
+                kidToggles$.removeClass('open')
+                this$.addClass('open');
+            }
+        }
+    }
+
+
+})(Layout);
+
+jQuery(document).ready(function () {    
+    Layout.Init();
+});
 
 })(jQuery);
