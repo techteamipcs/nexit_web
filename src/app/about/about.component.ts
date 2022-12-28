c44e7e55c499e2509d3945d939cd457bb1975b71
mohammed asad <asad.ipcs@gmail.com>
2022-12-28 11:47:56 +0530

    about page icon replacement

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  baseUrl:any;
+  constructor() {
+    this.baseUrl = environment.baseUrl + '/assets';
+   }
 
   ngOnInit(): void {
   }
