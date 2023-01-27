ef8967f950c9c47c06c007359aa52d802991b5f4
mohammed asad <asad.ipcs@gmail.com>
2023-01-27 10:03:34 +0530

    on hover image changes

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { DataService } from 'src/app/providers/data.service';
 import { Router } from '@angular/router';
-
+declare var $: any;
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -40,5 +40,24 @@ export class FooterComponent implements OnInit {
 
       },
     );
+  }
+  func_1(value:any){
+    if(value && value.sequence_number == 1){
+      $(".cat_"+value.sequence_number).attr("src", this.baseUrl + "/source/images/icon/server-green.svg");
+    } else if(value && value.sequence_number == 2){
+      $(".cat_"+value.sequence_number).attr("src", this.baseUrl + "/source/images/icon/networking-green1.svg");
+    } else if(value && value.sequence_number == 3){
+      $(".cat_"+value.sequence_number).attr("src", this.baseUrl + "/source/images/icon/laptop-green.svg");
+    } else if(value && value.sequence_number == 4){
+      $(".cat_"+value.sequence_number).attr("src", this.baseUrl + "/source/images/icon/pcs-green.svg");
+    } else if(value && value.sequence_number == 5){
+      $(".cat_"+value.sequence_number).attr("src", this.baseUrl + "/source/images/icon/networking-green1.svg");
+    } 
+}
+
+  func_11(value:any){
+    if(value && value.sequence_number){
+    $(".cat_"+value.sequence_number).attr("src", this.backendUrl + "/" + value.icon);
+    }
   }
 }
